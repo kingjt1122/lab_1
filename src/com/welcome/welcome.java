4978784ceee10362915124eacf69406167497b13
Jingtao Wang <542286866@qq.com>
2017-09-25 13:33:35 +0800

    c4 change 4 files

--- src/com/welcome/welcome.java
+++ src/com/welcome/welcome.java
@@ -124,7 +124,7 @@ public class welcome extends JFrame{
 		
 		String choice="0";
 		
-		System.out.println("1:show the Graph.  2:search bridge words.  3:build new text.  4:shortest path.  5:random walk\n");
+		System.out.println("choices: 1:show the Graph.  2:search bridge words.  3:build new text.  4:shortest path.  5:random walk\n");
 		choice=sc.nextLine();
 		if(choice.equals("1")){
 			showDirectedGraph(mtx);
